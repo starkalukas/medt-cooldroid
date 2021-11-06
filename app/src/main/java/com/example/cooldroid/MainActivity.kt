08ef9f87f894ed33b116c1649c5098036eeed698
StarkaLukas <lstarka141000@gmail.com>
2021-11-07 00:35:53 +0100

    implement counter to display how often you touched the droid

--- app/src/main/java/com/example/cooldroid/MainActivity.kt
+++ app/src/main/java/com/example/cooldroid/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.cooldroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        cooldroidText.text = "New way"
+        imageView.setOnClickListener{
+            counter++
+            Log.d(null, "onCreate(), counter: ${counter}")
+            cooldroidText.text =
+                when (counter) {
+                    0 -> getString(R.string.txt_dont_touch)
+                    1 -> getString(R.string.once)
+                    2 -> getString(R.string.twice)
+                    else -> getString(R.string.text_view_numbers, counter)
+                }
+        }
     }
 }
