e29300307925bb65b2b815fa75f2ca5e87b2f681
StarkaLukas <lstarka141000@gmail.com>
2021-11-09 14:56:31 +0100

    save state of counter

--- app/src/main/java/com/example/cooldroid/MainActivity.kt
+++ app/src/main/java/com/example/cooldroid/MainActivity.kt
@@ -6,20 +6,35 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
-    var counter = 0
+    private var counter = 0
+    private val COUNTER = "counter"
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(COUNTER)
+            cooldroidText.text = getMessageText()
+        }
         imageView.setOnClickListener{
             counter++
-            Log.d(null, "onCreate(), counter: ${counter}")
-            cooldroidText.text =
-                when (counter) {
-                    1 -> getString(R.string.once)
-                    2 -> getString(R.string.twice)
-                    else -> getString(R.string.text_view_numbers, counter)
-                }
+            Log.d(LOG_TAG, "onCreate(), counter: ${counter}")
+            cooldroidText.text = getMessageText()
+
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNTER, counter)
+        Log.d(LOG_TAG, "onSaveInstanceState(), counter: ${counter}")
+        super.onSaveInstanceState(outState)
+    }
+
+    fun getMessageText() =
+        when (counter) {
+            1 -> getString(R.string.once)
+            2 -> getString(R.string.twice)
+            else -> getString(R.string.text_view_numbers, counter)
+        }
 }
